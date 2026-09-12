6755bc919fd790b99d7c608be7fa52cbe701ac3e
ArchitectureWorld <140295054+ArchitectureWorld@users.noreply.github.com>
2026-09-12 11:36:12 +0800

    feat: export traceable research registry

---
+++ src/research/index.ts
@@ -0,0 +1,19 @@
+export { ResearchRegistry } from './registry.ts'
+export type {
+  AnalysisTrace,
+  AnalysisTraceStep,
+  DataSourceDefinition,
+  EvidenceRecord,
+  ResearchAccessMode,
+  ResearchAuthorityLevel,
+  ResearchDataPoint,
+  ResearchFreshnessPolicy,
+  ResearchMethodDefinition,
+  ResearchMinimumEvidence,
+  ResearchQueryTemplate,
+  ResearchReliabilityGrade,
+  ResearchSourcePreference,
+  ResearchSourcePriority,
+  ResearchValidationResult,
+  WorkflowResearchSpec,
+} from './types.ts'
